fe31c2f083d7eb79510b3319f0fdc85107dde94b
Rogelio Long <rlong021@gmail.com>
2024-10-06 14:46:54 -0600

    added a backpack that looks like a fridge and made it so collectables look like they are going into the backpack.

--- hud/hud.gd
+++ hud/hud.gd
@@ -8,16 +8,15 @@ func _ready() -> void:
 
 
 func start_day(dayLength):
-	$Container/DayTimeTracker/Path2D/PathFollow2D.progress_ratio = 0
-	$Container/DayTimeTracker/DayTimer.wait_time = dayLength/100.0
-	$Container/DayTimeTracker/DayTimer.start()
-	$Container/DayTimeTracker/AnimatedSprite2D.play()
-	
-	pass
+	$ContDTT/DayTimeTracker/Path2D/PathFollow2D.progress_ratio = 0
+	$ContDTT/DayTimeTracker/DayTimer.wait_time = dayLength/100.0
+	$ContDTT/DayTimeTracker/DayTimer.start()
+	$ContDTT/DayTimeTracker/AnimatedSprite2D.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	
 	pass
 	
@@ -28,21 +27,27 @@ func _on_player_stamina_changed(stamina_changed,maxStamina) -> void:
 
 func _on_player_health_changed(currentHealth,maxHealth) -> void:
 	$HPProgressBar.value = 100*currentHealth/maxHealth
-	pass # Replace with function body.
 
 
 func _on_day_timer_timeout() -> void:
-	$Container/DayTimeTracker/Path2D/PathFollow2D.progress_ratio += .01
-	$Container/DayTimeTracker/AnimatedSprite2D.position = $Container/DayTimeTracker/Path2D/PathFollow2D.position
+	$ContDTT/DayTimeTracker/Path2D/PathFollow2D.progress_ratio += .01
+	$ContDTT/DayTimeTracker/AnimatedSprite2D.position = $ContDTT/DayTimeTracker/Path2D/PathFollow2D.position
 
 
 func _on_player_gathered(colname) -> void:
-	print("here")
-	var new_col = collected.instantiate()
-	new_col.position = Vector2(200,200)
 	
-	#var new_bullet = playerBullet.instantiate()
-		#new_bullet.position = $Marker2D.global_position
-		#new_bullet.direction = lastlook.normalized()
-		#add_sibling(new_bullet)
-	pass # Replace with function body.
+	$ContBp/AnimatedSprite2D2.animation = colname
+	$ContBp/AnimatedSprite2D2.show()
+	for i in 6:
+		$ContBp/AnimatedSprite2D2.play()
+		$ContBp/Path2D/PathFollow2D.progress_ratio = i/5.0
+		$ContBp/AnimatedSprite2D2.position = $ContBp/Path2D/PathFollow2D.position
+		await get_tree().create_timer(0.01).timeout
+	$ContBp/AnimatedSprite2D2.hide()
+		
+	
+		
+	print(colname)
+	
+	
+	
