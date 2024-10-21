2d5af92538ae9ca50e2e449ec3b44a5cc068e9cc
requestforcriticism <41657338+requestforcriticism@users.noreply.github.com>
2024-10-20 22:58:34 -0600

    added tech tree to night phase screen
    build for demo 1

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
-echo "TODO: build script to other branch"
+@echo off
+
+echo "Deleting Old Build"
+
+del docs\* /Q
+
+echo "Building Web Files"
+
+..\bin\godot\Godot_v4.3-stable_win64.exe --export-debug Web --no-window
 
 PAUSE
